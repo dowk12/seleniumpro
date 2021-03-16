315d185687b50bd88fe5e674d9d1c9203ca8da02
dowk12 <stonerl12@naver.com>
2021-03-16 23:10:58 +0900

    #2.2 Recap and Screenshots

--- responsive.py
+++ responsive.py
@@ -10,13 +10,15 @@ browser = webdriver.Chrome(ChromeDriverManager().install())
 browser.get("https://nomadcoders.co")
 browser.maximize_window()
 
-sizes = [320, 480, 960, 1366, 1920]
+sizes = [480, 960, 1366, 1920]
 
 for size in sizes:
     browser.set_window_size(size, BROWSER_HEIGHT)
+    browser.execute_script("window.scrollTo(0, 0)")
     time.sleep(3)
     scroll_size = browser.execute_script("return document.body.scrollHeight")
     total_sections = ceil(scroll_size / BROWSER_HEIGHT)
     for section in range(total_sections):
-        browser.execute_script(f"window.scrollTo(0, {(section+1) * BROWSER_HEIGHT})")
+        browser.execute_script(f"window.scrollTo(0, {section * BROWSER_HEIGHT})")
+        browser.save_screenshot(f"screenshots/{size}x{section}.png")
         time.sleep(2)
